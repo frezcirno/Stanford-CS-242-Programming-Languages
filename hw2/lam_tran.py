24ca47663559cf7c3cc3261fad9cce9f0133755f
Zixuan Tan <tanzixuan.me@gmail.com>
2025-02-07 23:34:07 +0800

    hw2 pass

--- hw2/lam_tran.py
+++ hw2/lam_tran.py
@@ -7,7 +7,32 @@ import src.ski as ski
 # TASK: Implement the below function `tran`.
 # You can define helper functions outside `tran` and use them inside `tran`.
 
+
+def tranu(s: str, e: ski.Expr) -> ski.Expr:
+    # U(x, I) = K I
+    # U(x, S) = K S
+    # U(x, K) = K K
+    if isinstance(e, (ski.I, ski.S, ski.K)):
+        return ski.App(ski.K(), e)
+    # U(x, x) = I
+    elif isinstance(e, ski.Var) and e.s == s:
+        return ski.I()
+    # U(x, y) = K y
+    elif isinstance(e, ski.Var):
+        return ski.App(ski.K(), e)
+    # U(x, (e1 e2)) = S (U(x, e1)) (U(x, e2))
+    elif isinstance(e, ski.App):
+        return ski.App(ski.App(ski.S(), tranu(s, e.e1)), tranu(s, e.e2))
+    raise RuntimeError
+
+
 def tran(e: lam.Expr) -> ski.Expr:
     # BEGIN_YOUR_CODE
-    return ski.I()
+    if isinstance(e, lam.Var):
+        return ski.Var(e.s)
+    elif isinstance(e, lam.Lam):
+        return tranu(e.s, tran(e.e))
+    elif isinstance(e, lam.App):
+        return ski.App(tran(e.e1), tran(e.e2))
+    raise RuntimeError
     # END_YOUR_CODE
--- hw2/ski_eval.py
+++ hw2/ski_eval.py
@@ -3,8 +3,50 @@ import src.ski as ski
 ##########
 # COPY YOUR IMPLEMENTATION FROM HW1
 ##########
+# TASK: Implement the below function `eval`.
+
+
+class Kx(ski.Expr):
+    def __init__(self, x):
+        self.x = x
+
+
+class Sx(ski.Expr):
+    def __init__(self, x):
+        self.x = x
+
+
+class Sxy(ski.Expr):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 def eval(e: ski.Expr) -> ski.Expr:
     # BEGIN_YOUR_CODE
+    if isinstance(e, ski.App):
+        e1 = eval(e.e1)
+        # I x => x
+        if isinstance(e1, ski.I):
+            return eval(e.e2)
+        # K x => Kx
+        elif isinstance(e1, ski.K):
+            return Kx(eval(e.e2))
+        # Kx y => x
+        elif isinstance(e1, Kx):
+            return e1.x  # e1.x is already evaluated
+        # S x => Sx
+        elif isinstance(e1, ski.S):
+            return Sx(eval(e.e2))
+        # Sx y => Sxy
+        elif isinstance(e1, Sx):
+            return Sxy(e1.x, eval(e.e2))
+        # Sxy z = x z (y z)
+        elif isinstance(e1, Sxy):
+            z = eval(e.e2)
+            xz = eval(ski.App(e1.x, z))
+            yz = eval(ski.App(e1.y, z))
+            return eval(ski.App(xz, yz))
     return e
+
     # END_YOUR_CODE
